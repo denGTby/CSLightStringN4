899a5fadfdbc25599282ce18660a83786ae28e23
dengt_by dengt_by <denis.wse.new@gmail.com>
2023-04-10 16:25:09 +0300

    Добавьте файлы проекта.

---
+++ CSLightStringN4/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text;
+
+namespace CSLightStringN4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            uint age;
+            string educationalInstitution;
+            decimal money;
+            string conclusion;
+
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+
+            Console.Write("Как вас зовут? ");
+            name = Console.ReadLine();
+
+            Console.Write("Сколько вам лет? ");
+            age = Convert.ToUInt32(Console.ReadLine());
+
+            Console.Write("Где вы учитесь? ");
+            educationalInstitution = Console.ReadLine();
+
+            Console.Write("Сколько у вас есть денег? ");
+            money = Convert.ToDecimal(Console.ReadLine());
+
+            conclusion = $"Вас зовут {name}. Ваш возраст {age}. " +
+                $"Вы учитесь в {educationalInstitution}. А денег у вас {money}. " +
+                $"{name}, вы молодец!";
+
+            Console.WriteLine(conclusion);
+            Console.ReadLine();
+        }
+    }
+}
